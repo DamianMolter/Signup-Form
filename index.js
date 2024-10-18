84687dc7aa5e1b6e29b220b795138f1cf62df770
Damian Molter <damian.molter.programista@gmail.com>
2024-10-18 14:41:22 +0200

    Added input boxes and error marks

---
+++ index.js
@@ -0,0 +1,3 @@
+      $(".green-button").click(function(){
+                  $(".input-box input").after("<img src = 'images/icon-error.svg'/> <br/> <span class = 'warning'>This field cannot be empty!</span>");          
+      })
