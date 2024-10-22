64a0ff3fd51bad55ef95168c290193f4d572a27e
Damian Molter <damian.molter.programista@gmail.com>
2024-10-22 14:13:14 +0200

    Fixed sme JS

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-      $(".green-button").click(function(){
-                  $(".input-box input").after("<img src = 'images/icon-error.svg'/> <br/> <span class = 'warning'>This field cannot be empty!</span>");          
-      })
+var numberOfClicks = 0;
+$(".green-button").click(function () {
+
+      if (numberOfClicks < 1) {
+            $(".input-box input").after("<img src = 'images/icon-error.svg'/> <br/> <span class = 'warning'>This field cannot be empty!</span>");
+
+            numberOfClicks++;
+      }
+})
