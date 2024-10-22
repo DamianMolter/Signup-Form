8b3a1de82c1ac0f11aa7e6f84a43827502b8d5fa
Damian Molter <damian.molter.programista@gmail.com>
2024-10-22 14:49:01 +0200

    Now fields better respond on written content

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-var numberOfClicks = 0;
+var firstNameClicks = lastNameClicks = emailClicks = passwordClicks = 0;
+
+$(this).bind("change paste", function(event){
+            $(this).val(event);
+});
+
 $(".green-button").click(function () {
 
-      if (numberOfClicks < 1) {
-            $(".input-box input").after("<img src = 'images/icon-error.svg'/> <br/> <span class = 'warning'>This field cannot be empty!</span>");
+            if($("#first-name").val() === "" && firstNameClicks < 1){
+                  $("#first-name").after("<img src = 'images/icon-error.svg'/> <br/> <span class = 'warning'>This field cannot be empty!</span>");
+                  firstNameClicks++; 
+            }
 
-            numberOfClicks++;
+            if($("#last-name").val() === "" && lastNameClicks < 1){
+                  $("#last-name").after("<img src = 'images/icon-error.svg'/> <br/> <span class = 'warning'>This field cannot be empty!</span>");
+                  lastNameClicks++;
+            }
+
+            if($("#email").val() === "" && emailClicks < 1){
+                  $("#email").after("<img src = 'images/icon-error.svg'/> <br/> <span class = 'warning'>This field cannot be empty!</span>"); 
+                  emailClicks++;
+            }
+
+            if($("#password").val() === "" && passwordClicks < 1){
+                  $("#password").after("<img src = 'images/icon-error.svg'/> <br/> <span class = 'warning'>This field cannot be empty!</span>");
+                  passwordClicks++;
+            }
       }
-})
+)
